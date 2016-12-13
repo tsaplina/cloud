f2fae04bc757a2064e44ecae71b2d45c49598e68
paskevich <artempaskevich@gmail.com>
2016-12-13 16:30:04 +0300

    опрос

---
+++ script.php
@@ -0,0 +1,20 @@
+<?php
+     header("Content-Type: text/html; charset=utf8");
+?>
+<?php 
+$name = $_POST ['name']);
+echo' <html>
+<head>
+	<meta content="html/text; charset=utf-8">
+	<link href="style.css" rel="stylesheet"/>
+		<title>
+			Спасибо за ответ
+		</title>
+</head>
+<body>
+    <div>
+        <p>Text</p>
+    </div>
+</body>
+</html>';
+?>
